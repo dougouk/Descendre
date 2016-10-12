d755320bc15748222ebfe113ff115090b94ff14d
Dongoug <dougouk@gmail.com>
2016-10-11 23:43:53 -0400

    Added a test button

--- app/src/main/java/com/dan190/descendre/MapsActivity.java
+++ app/src/main/java/com/dan190/descendre/MapsActivity.java
@@ -1,18 +1,31 @@
 package com.dan190.descendre;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.CancelableCallback;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMapClickListener{
 
     private GoogleMap mMap;
+
+    public static final CameraPosition MONTREAL =
+            new CameraPosition.Builder().target(new LatLng(45.5, -73.6))
+                    .zoom(15.5f)
+                    .bearing(0)
+                    .tilt(0)
+                    .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +50,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng montreal = new LatLng(45.5, -73.6);
+        // Add a marker in Sydney and move the camera
+        /*LatLng montreal = new LatLng(45.5, -73.6);
         mMap.addMarker(new MarkerOptions().position(montreal).title("Marker in Montreal"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(montreal));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(montreal));*/
+
+        mMap.addMarker(new MarkerOptions().position(montreal).title("Marker in Montreal"));
+
+        changeCamera(CameraUpdateFactory.newCameraPosition(MONTREAL));
+    }
+
+    @Override
+    public void onMapClick(LatLng point){
+        Toast.makeText(getApplicationContext(),
+                "Clicked " + point.toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onGoToMontreal(View v){
+        changeCamera(CameraUpdateFactory.newCameraPosition(MONTREAL));
+    }
+
+    private void changeCamera(CameraUpdate update) {
+        changeCamera(update, null);
+    }
+
+    /**
+     * Change the camera position by moving or animating the camera depending on the state of the
+     * animate toggle button.
+     */
+    private void changeCamera(CameraUpdate update, CancelableCallback callback) {
+        mMap.moveCamera(update);
     }
 }
