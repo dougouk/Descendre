88f0a36affb41179f5148b5d0cfd9a854fbf2344
Dongoug <dougouk@gmail.com>
2016-10-13 20:04:14 -0400

    Displays circle around initial marker

--- app/src/main/java/com/dan190/descendre/MapsActivity.java
+++ app/src/main/java/com/dan190/descendre/MapsActivity.java
@@ -3,6 +3,7 @@ package com.dan190.descendre;
 import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -18,6 +19,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -26,6 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private UiSettings mUiSettings;
+    private Circle circle;
 
 
     public static final CameraPosition MONTREAL =
@@ -34,6 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .bearing(0)
                     .tilt(0)
                     .build();
+
+    public static final LatLng MONTREAL_LL = new LatLng(45.5, -73.6);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +51,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -67,6 +63,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMyLocationEnabled(true);
 
         mUiSettings.setMapToolbarEnabled(true);
+
+        circle = mMap.addCircle(new CircleOptions()
+                .center(MONTREAL_LL)
+                .radius(200)
+                .strokeColor(Color.BLACK)
+                .fillColor(0x00000000));
 
         LatLng montreal = new LatLng(45.5, -73.6);
         // Add a marker in Sydney and move the camera
