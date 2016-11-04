cb86e8f9828d10faf0b4ffb5c101f6c90dd2a1bb
Dongoug <dougouk@gmail.com>
2016-11-04 09:46:19 -0400

    Adding UserState Enums

---
+++ app/src/main/java/com/dan190/descendre/UserState.java
@@ -0,0 +1,12 @@
+package com.dan190.descendre;
+
+/**
+ * Created by Dan on 03/11/2016.
+ */
+
+public enum UserState {
+    NORMAL,
+    SELECTING_MARKER,
+    ADDING_MARKER,
+    DELETING_MARKER
+}
