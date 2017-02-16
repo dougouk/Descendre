45fe79056eba9e2c2e1c92efd2a5a7b4f6a06ca4
Dongoug <dougouk@gmail.com>
2017-02-16 01:57:55 -0500

    Construction

--- app/src/main/java/com/dan190/descendre/TabLayoutActivity.java
+++ app/src/main/java/com/dan190/descendre/TabLayoutActivity.java
@@ -2,18 +2,17 @@ package com.dan190.descendre;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentTransaction;
+import android.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TableLayout;
 
 import com.dan190.descendre.AlarmMonitor.AlarmMonitorFragment;
 import com.dan190.descendre.Geofence.MyGeofence;
 import com.dan190.descendre.Map.MapFragment;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Dan on 11/11/2016.
@@ -21,8 +20,14 @@ import java.util.Map;
 
 public class TabLayoutActivity extends AppCompatActivity
         implements MapFragment.OnGeofenceListener,
-AlarmMonitorFragment.OnAlarmMonitorListener{
-    private static String ACTIVITY_NAME = "TabLayoutActivity";
+                    AlarmMonitorFragment.OnAlarmMonitorListener
+{
+    private static String TAG = TabLayoutActivity.class.getName();
+
+
+
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,20 +41,29 @@ AlarmMonitorFragment.OnAlarmMonitorListener{
         tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.settings));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager = (ViewPager) findViewById(R.id.pager);
         final PagerAdapter adapter = new PagerAdapter
-                (getSupportFragmentManager(), tabLayout.getTabCount());
+                (getFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+                Log.i(TAG, tab.toString() + " selected");
+//                android.app.Fragment fragment = TabLayoutActivity.this.getFragmentManager()
+//                        .findFragmentByTag(MapFragment.mapFragmentKey);
+//                if(fragment!= null){
+//                    Log.d(TAG, "Fragment is not null");
+//                    TabLayoutActivity.this.getFragmentManager().beginTransaction()
+//                            .remove(fragment);
+//                }
+
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-                Log.i(ACTIVITY_NAME, "onTabUnselected()");
+                Log.i(TAG, "onTabUnselected()");
             }
 
             @Override
@@ -59,21 +73,25 @@ AlarmMonitorFragment.OnAlarmMonitorListener{
         });
     }
 
+    public ViewPager getViewPager() {
+        return viewPager;
+    }
+
     @Override
     public void onCreateGeofence(List<MyGeofence> myGeofenceList) {
         Log.d("TabLayoutAcitivty", "onCreateGeofence()");
 
         AlarmMonitorFragment alarmMonitorFragment = (AlarmMonitorFragment)
-                getSupportFragmentManager().findFragmentById(R.id.alarmMonitorFragment);
+                getFragmentManager().findFragmentById(R.id.alarmMonitorFragment);
 
         if(alarmMonitorFragment == null){
-            Log.w(ACTIVITY_NAME, "alarmMonitorFragment is null");
+            Log.w(TAG, "alarmMonitorFragment is null");
 
             alarmMonitorFragment = new AlarmMonitorFragment();
             Bundle args = new Bundle();
             //??
 
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
             transaction.replace(R.id.alarmMonitorFragment, alarmMonitorFragment);
             transaction.addToBackStack(null);
@@ -91,22 +109,23 @@ AlarmMonitorFragment.OnAlarmMonitorListener{
 
     @Override
     public void removeAllGeofences() {
-        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        if(mapFragment == null){
-            Log.w(ACTIVITY_NAME, "mapFragment is null");
-
-            mapFragment = new MapFragment();
-            Bundle args = new Bundle();
-            //??
-
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-            transaction.replace(R.id.map, mapFragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
-        else{
-            mapFragment.clearAll();
-        }
+        //TODO
+//        MapFragment mapFragment = null;
+//        if(mapFragment == null){
+//            Log.w(TAG, "mapFragment is null");
+//
+//            mapFragment = new MapFragment();
+//            Bundle args = new Bundle();
+//            //??
+//
+//            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//
+//            transaction.replace(R.id.frameContainer, mapFragment);
+//            transaction.addToBackStack(null);
+//            transaction.commit();
+//        }
+//        else{
+//            mapFragment.clearAll();
+//        }
     }
 }
