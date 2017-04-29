5224c1232b74388cf871743af29e7235df13ebb6
Dongoug <dougouk@gmail.com>
2017-04-29 18:28:55 -0400

    Adding Helper Classes for bus stop

---
+++ app/src/main/java/com/dan190/descendre/Helpers/BusStopJSON.java
@@ -0,0 +1,8 @@
+package com.dan190.descendre.Helpers;
+
+/**
+ * Created by Dan on 29/04/2017.
+ */
+
+public class BusStopJSON {
+}
---
+++ app/src/main/java/com/dan190/descendre/Util/BusStop.java
@@ -0,0 +1,8 @@
+package com.dan190.descendre.Util;
+
+/**
+ * Created by Dan on 29/04/2017.
+ */
+
+public class BusStop {
+}
