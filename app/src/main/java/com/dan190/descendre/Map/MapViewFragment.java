ee1fe386d519bd11be12c9f66c8d7365f3d72001
Dongoug <dougouk@gmail.com>
2017-04-29 18:34:49 -0400

    Bus stops appear around a static location

--- app/src/main/java/com/dan190/descendre/Map/MapViewFragment.java
+++ app/src/main/java/com/dan190/descendre/Map/MapViewFragment.java
@@ -2,11 +2,13 @@ package com.dan190.descendre.Map;
 
 import android.Manifest;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -18,13 +20,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.dan190.descendre.Geofence.MyGeofence;
+import com.dan190.descendre.Helpers.BusStopJSON;
 import com.dan190.descendre.R;
 import com.dan190.descendre.TabLayoutActivity;
+import com.dan190.descendre.Util.BusStop;
 import com.dan190.descendre.Util.UserState;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -37,6 +41,18 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.Context.LOCATION_SERVICE;
 
@@ -60,19 +76,30 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
     private UserState mUserState;
     private Marker chosenMarker;
     private Circle chosenCircle;
+    private List<BusStop> busStopList;
+    private List<Marker> busStopMarkers;
 
     /**
      * Connection variables
      */
     private GoogleApiClient mGoogleAPIClient;
 
+    /**
+     * Creating the view for the map
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_maps, container, false);
 
+        busStopList = new ArrayList<>();
+        busStopMarkers = new ArrayList<>();
+
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
-
         mMapView.onResume(); // needed to get the map to display immediately
 
         try {
@@ -89,7 +116,7 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
 
                 googleMap = mMap;
 
-                // For showing a move to my location button
+                // For showing a move-to-my-location button
                 if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                         && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
@@ -122,6 +149,12 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
 
                 mUserState = UserState.NORMAL;
                 assignButtons();
+
+                //Get Nearby Bus Stops
+                ParserTask parserTask = new ParserTask();
+                LatLng fort = new LatLng(45.4911845, -73.5814331);
+                parserTask.execute(BusStopJSON.
+                        getNearbyBusStops(fort, 300, getResources().getString(R.string.google_maps_key)));
             }
         });
 
@@ -129,6 +162,9 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
     }
 
 
+    /**
+     * The application needs to ask for permission to use user location on the device
+     */
     private void askForLocationPermission() {
         if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
             // Explain to the user why they need to provide location permission
@@ -145,6 +181,87 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
         }
     }
 
+    /**
+     * Bus Stop JSON Methods
+     */
+
+
+    private String retrieveJSON(String inputURL) throws IOException {
+        HttpURLConnection httpURLConnection = null;
+        InputStream inputStream = null;
+        String data = "";
+        try{
+            URL url = new URL(inputURL);
+
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+            inputStream = httpURLConnection.getInputStream();
+            BufferedReader bfr = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while((line = bfr.readLine()) != null){
+                stringBuffer.append(line);
+            }
+
+            data = stringBuffer.toString();
+            bfr.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(inputStream!=null) inputStream.close();
+            httpURLConnection.disconnect();
+        }
+        return data;
+    }
+
+
+    class ParserTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String data = "";
+            try {
+                data = retrieveJSON(params[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            if(result.isEmpty()) return;
+            try {
+                busStopList = BusStopJSON.parseJSON(result);
+                for(BusStop busStop : busStopList){
+                    busStopMarkers.add( googleMap.addMarker(new MarkerOptions()
+                            .position(busStop.getLatLng())
+                            .title(busStop.getName())));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Lifecycle stuff
+     */
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        try{
+            mDirectionRequestedListener = (onDirectionRequestedListener) getActivity();
+        }catch(ClassCastException e){
+            e.printStackTrace();
+        }
+    }
     @Override
     public void onResume() {
         super.onResume();
@@ -168,6 +285,7 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+
     /**
      * Refactored Methods
      */
@@ -224,6 +342,7 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
         getDirectionsButton.setVisibility(View.VISIBLE);
         mUserState = UserState.NORMAL;
     }
+
     /**
      * Listeners
      */
@@ -231,9 +350,16 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
     private View.OnClickListener getDirectionsListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            LatLng destination = chosenMarker.getPosition();
+            mDirectionRequestedListener.onDirectionRequested(new LatLng(mUserLocation.getLatitude(),
+                                                                        mUserLocation.getLongitude()),
+                                                            destination);
+
+            // Set Tab View to Destination Fragment
             TabLayoutActivity tabLayoutActivity = (TabLayoutActivity) getActivity();
             ViewPager viewPager = tabLayoutActivity.getViewPager();
             viewPager.setCurrentItem(1);
+
 
         }
     };
@@ -261,6 +387,17 @@ public class MapViewFragment extends Fragment implements GoogleApiClient.Connect
             return false;
         }
     };
+
+    /**
+     * Interfaces used to send data between fragments
+     */
+
+    private onDirectionRequestedListener mDirectionRequestedListener;
+    public interface onDirectionRequestedListener{
+        public void onDirectionRequested(LatLng origin, Place destination);
+        public void onDirectionRequested(LatLng origin, LatLng destination);
+    }
+
     /** Initialization stuff
      *
      */
