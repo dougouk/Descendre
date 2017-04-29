c7c9bf81bed293d5b60de518f8a0a77256c63fdc
Dongoug <dougouk@gmail.com>
2017-04-29 18:29:14 -0400

    Bus stop class

--- app/src/main/java/com/dan190/descendre/Util/BusStop.java
+++ app/src/main/java/com/dan190/descendre/Util/BusStop.java
@@ -1,8 +1,33 @@
 package com.dan190.descendre.Util;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by Dan on 29/04/2017.
  */
 
 public class BusStop {
+    LatLng latLng;
+    String name;
+
+    public BusStop(){}
+    public BusStop(LatLng latLng, String name){
+        this.latLng = latLng;
+        this.name = name;
+    }
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
